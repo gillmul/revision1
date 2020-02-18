98b4f6aed76d41e6287afb3318ec05f97f71b56e
gillmul <59933613+gillmul@users.noreply.github.com>
2020-02-18 13:29:15 -0500

    Update Revision.java

--- src/revision/Revision.java
+++ src/revision/Revision.java
@@ -16,7 +16,7 @@ public class Revision {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hello");
+        System.out.println("Gill");
     }
     
 }
